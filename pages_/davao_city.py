82550554ecf3bb6bba8dc307d4f984d0ab99deeb
beacmreyes <beacmreyes@gmail.com>
2024-08-18 00:33:20 +0800

    Add files via upload

--- pages_/davao_city.py
+++ pages_/davao_city.py
@@ -32,7 +32,14 @@ def main():
     col1, col2 = st.columns([3, 6])
 
     with col1:
-        st.write("About Davao City....")
+        # Display the quote
+        st.write("""
+        "Davao City is the regional center of Davao Region. It is the most populous city in Mindanao and the third largest city in the Philippines. Known for its bustling economic activities, urban build-up and modern amenities, Davao City is one of the most important economies in the island, and the third most important urban center in the Philippines."
+        """)
+        
+        # Provide the citation
+        st.markdown("<a href='https://philippines.travel/destinations/davao-city' target='_blank' style='font-size: 12px; text-decoration: none; color: inherit'> - Official Website of the Department of Tourism, Philippines</a>", unsafe_allow_html=True)
+
 
 
     with col2:
